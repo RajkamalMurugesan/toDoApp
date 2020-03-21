32b4991b6194a2b1eaeebc6f1ce0d85bfcf07230
Rajkamal <rajkamalmurugasean@gmail.com>
2020-03-21 19:34:49 +0530

    toDoApp commit

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,18 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebase : {
+    apiKey: "AIzaSyD2L1ljLajXEkvUXXOYeRnds3eqiQiolIM",
+    authDomain: "todoapp-52a96.firebaseapp.com",
+    databaseURL: "https://todoapp-52a96.firebaseio.com",
+    projectId: "todoapp-52a96",
+    storageBucket: "todoapp-52a96.appspot.com",
+    messagingSenderId: "936897361266",
+    appId: "1:936897361266:web:f3a9e45469637ee33359eb",
+    measurementId: "G-7GQ73BMN4N"
+
+  } 
 };
 
 /*
